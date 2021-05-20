7850dc944309d932c1a42bd300386f215b0efb60
hussain asad <mianhussainasad@gmail.com>
2021-05-20 21:13:17 +0500

    sold model and product model

--- index.js
+++ index.js
@@ -130,6 +130,9 @@ app.set('view engine', 'ejs');
         res.render('DataBaseEntryFormPage');
     });
     
+
+
+
 // zeenia's code
  app.get('/Sales-Sheet', (req, res) => {
      SoldProducts.find({}, (err, products)=>{
@@ -137,3 +140,8 @@ app.set('view engine', 'ejs');
          res.render('Sales-Sheet', {});
      })
  });
+
+
+ app.post('/userLogin', (req, res)=>{
+     
+ })
